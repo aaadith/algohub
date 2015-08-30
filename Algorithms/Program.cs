0f16a5c07990723867a170d878123eeb634f4ec8
Aadith <aaadith@outlook.com>
2015-08-30 10:35:47 -0700

    Moving ML and computational geometry to self-contained projects

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Algorithms.Selection;
-using Algorithms.Sorting;
 using System.Collections;
-using Algorithms.ComputationalGeometry;
-using Algorithms.MachineLearning.DecisionTree;
 
 namespace Algorithms
 {
@@ -16,7 +12,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            ID3Client.Run();
+
         }
     }
 }
