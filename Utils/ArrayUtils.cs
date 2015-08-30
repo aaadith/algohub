8b7161e1003c777ac55ed1b2652263a22b39baec
Aadith <aaadith@gmail.com>
2015-08-30 14:12:47 -0700

    updating Utils

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Algorithms
+namespace Utils
 {
     public static class ArrayUtils
     {
---
+++ Utils/IntUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class IntUtils
+    {
+        public static int GetNumDigits(this int x)
+        {
+            return (int)Math.Log10(x) + 1;
+        }
+    }
+}
