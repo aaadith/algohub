0135a718a815358cadedb126db055bcb2c6ef4d3
Aadith <aaadith@gmail.com>
2015-09-24 22:26:39 -0700

    adding CSharp project

---
+++ CSharp/Linq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Linq
+{
+    public class SomeClass
+    {
+        public int A { get; set; }
+
+        public int B { get; set; }
+    }
+    public class LinqUsage
+    {
+        public static void GetMax()
+        {
+            SomeClass a = new SomeClass { A = 3, B = 2 };
+            SomeClass b = new SomeClass { A = 1, B = 2 };
+            SomeClass c = new SomeClass { A = 2, B = 2 };
+            SomeClass d = new SomeClass { A = 4, B = 2 };
+
+
+            SomeClass[] l = new SomeClass[] { a, b, c, d };
+            List<SomeClass> s = l.Where(x=>x.B==2).ToList<SomeClass>();
+            
+            int? max = s.Max(x => x.A);
+            Console.WriteLine(max);
+
+        }
+    }
+}
---
+++ CSharp/ListInCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+
+    
+    public class ListComparer<T> : IEqualityComparer<List<T>>
+    {
+
+        public bool Equals(List<T> x, List<T> y)
+        {
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(List<T> obj)
+        {
+            int hashcode = 0;
+            foreach (T t in obj)
+            {
+                hashcode ^= t.GetHashCode();
+            }
+            return hashcode;
+        }
+    }
+
+    public class ListInCollection
+    {
+        static void ListInHashSet()
+        {
+            List<int> a = new List<int> { 1, 2, 3 };
+            List<int> b = new List<int> { 1, 2, 3 };
+
+            HashSet<List<int>> h = new HashSet<List<int>>(new ListComparer<int>());
+            h.Add(a);
+
+            bool x = h.Contains(b);
+
+        }
+
+        static void ListAsDictionaryKey()
+        {
+            List<int> a = new List<int> { 1, 2, 3 };
+            List<int> b = new List<int> { 1, 2, 3 };
+
+
+
+            Dictionary<List<int>, int> d = new Dictionary<List<int>, int>(new ListComparer<int>());
+            d[a] = 1;
+
+            bool x = d.ContainsKey(b);
+
+        }
+
+
+    }
+}
