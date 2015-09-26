c6c6fb8db4d98fa5c24283da1237c749bdaade91
Aadith <aaadith@gmail.com>
2015-09-25 19:09:46 -0700

    adding params

---
+++ CSharp/Params.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Params
+    {
+        void MethodTakingVariableNumberOfArguments(int someArg, params int[] moreArgs)
+        {
+            int sum = someArg;
+            foreach(int x in moreArgs)
+            {
+                sum += x;
+            }
+        }
+
+        void Client()
+        {
+            MethodTakingVariableNumberOfArguments(1, 2, 3, 4, 5);
+        }
+    }
+}
