0720e839ffcdc3434695b5d09ff4f9cfe1b0e1d2
Aadith <aaadith@gmail.com>
2015-10-16 18:43:57 -0700

    adding ProjectEuler

---
+++ ProjectEuler/P59/P59Base.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.P59
+{
+    public class P59Base
+    {
+        protected string GetCipherText()
+        {
+            string line = File.ReadAllLines("P59\\cipher.txt").First();
+            string[] codes = line.Split(',');
+            char[] x = Array.ConvertAll(codes, code => (char)Int32.Parse(code));
+            string cipherText = new string(x);
+            return cipherText;
+        }
+
+        protected HashSet<string> GetDictionary()
+        {
+            string[] lines = File.ReadAllLines("P59\\CommonEnglishWords.txt");
+            HashSet<string> dictionary = new HashSet<string>(lines);
+            return dictionary;
+        }
+
+        protected string Decrypt(string cipherText, string key)
+        {
+            char[] decryptedChars = new char[cipherText.Length];
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                decryptedChars[i] = (char)(cipherText[i] ^ key[i % key.Length]);
+            }
+
+            string result = new string(decryptedChars);
+            return result;
+        }
+
+        protected float GetPercentMatch(string text, HashSet<string> dictionary)
+        {
+            float result = 0;
+
+            int total = text.Length;
+            int matched = 0;
+            char[] separators = new char[] { ' ', ',', '.', '?', '!', '(', ')' };
+            matched = text.Count(x => separators.Contains(x));
+            foreach (string word in text.Split(separators))
+            {
+                if (dictionary.Contains(word.ToLower()))
+                    matched += word.Length;
+            }
+            result = matched * 1.0f / total;
+            return result;
+        }
+
+    }
+}
---
+++ ProjectEuler/P59/P59_Parallel1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.P59
+{
+    public class P59_Parallel1 : P59Base
+    {
+        static List<string> GenerateAllKeys()
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    for (int k = 0; k < 26; k++)
+                    {
+                        string key = new string(new char[] { (char)(i + 97), (char)(j + 97), (char)(k + 97) });
+                        result.Add(key);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void FindKey()
+        {
+            string cipherText = GetCipherText();
+            HashSet<string> dictionary = GetDictionary();
+            List<string> keys = GenerateAllKeys();
+
+            Stopwatch sw = new Stopwatch();
+
+            object syncroot = new object();
+
+            sw.Start();
+            string decryptedText = null;
+            float resultScore = 0.0f;
+            Parallel.For(0, keys.Count, (i) =>
+            {
+                string key = keys[i];
+                string result = Decrypt(cipherText, key);
+                float score = GetPercentMatch(result, dictionary);
+                if (score > resultScore)
+                {
+                    lock (syncroot)
+                    {
+                        if (score > resultScore)
+                        {
+                            decryptedText = result;
+                            resultScore = score;
+                        }
+                    }
+                }
+            });
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+        }
+    }
+}
